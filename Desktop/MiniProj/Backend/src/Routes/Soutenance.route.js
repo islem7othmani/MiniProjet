24094ca497b7d75a571dfac83754afcaef12a2ab
islem othmani <96685631+islem7othmani@users.noreply.github.com>
2025-04-16 00:07:40 +0200

    Delete Desktop directory

--- Desktop/MiniProj/Backend/src/Routes/Soutenance.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const soutenanceController = require("../Controllers/Soutenance.controller");
-
-router.get("/getAll", soutenanceController.getAllSoutenances);
-router.post("/create", soutenanceController.createSoutenance);
-router.put("/update/:id_s", soutenanceController.updateSoutenance);
-
-module.exports = router;
